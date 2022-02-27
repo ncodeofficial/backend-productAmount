073e8f0150d0e19b778e60702d63df2bae224f14
sinseungmi <tlstmdal3@naver.com>
2022-02-28 03:00:23 +0900

    최종 할인금액 산출 API 구현

--- src/main/java/dcode/repository/ProductRepository.java
+++ src/main/java/dcode/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package dcode.repository;
 
 import dcode.domain.entity.Product;
+import dcode.domain.entity.Promotion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,8 +13,8 @@ public class ProductRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public Product getProduct(int id) {
-        String query = "SELECT * FROM `product` WHERE id = :id ";
-
+        String query = "SELECT * FROM product WHERE id = :id ";
+        
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("id", id);
 
@@ -27,4 +28,33 @@ public class ProductRepository {
                         .build()
         );
     }
+    
+    public Promotion getPromotion(int[] promotion_id) {
+    	String promotion_query = "SELECT pro.id,pro.promotion_type, pro.name, pro.discount_type, "
+        		+ "pro.discount_value, pro.use_started_at, pro.use_ended_at "
+        		+ "FROM promotion pro inner join promotion_products using (id) "
+        		+ "WHERE pro.id = :id ";
+
+    	int promotion_number = (int)(Math.random() * promotion_id.length) + 1;
+    	
+    	MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", promotion_number);
+        
+        return  namedParameterJdbcTemplate.queryForObject(
+        		promotion_query,
+                params,
+                (rs, rowNum) -> Promotion.builder()
+                		.id(rs.getInt("id"))
+                		.promotion_type(rs.getString("promotion_type"))
+                		.name(rs.getString("name"))
+                		.discount_type(rs.getString("discount_type"))
+                		.discount_value(rs.getInt("discount_value"))
+                		.use_started_at(rs.getDate("use_started_at"))
+                		.use_ended_at(rs.getDate("use_ended_at"))
+                		.build()
+                        
+        );
+        
+    }
+    
 }
--- src/main/java/dcode/service/ProductService.java
+++ src/main/java/dcode/service/ProductService.java
@@ -1,6 +1,7 @@
 package dcode.service;
 
 import dcode.domain.entity.Product;
+import dcode.domain.entity.Promotion;
 import dcode.model.request.ProductInfoRequest;
 import dcode.model.response.ProductAmountResponse;
 import dcode.repository.ProductRepository;
@@ -14,9 +15,37 @@ public class ProductService {
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request){
         System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
-
+        
         Product product = repository.getProduct(request.getProductId());
-
-        return null;
+        Promotion promotion = repository.getPromotion(request.getCouponIds());
+        int price = product.getPrice();
+        int finalPrice = 0;
+        int discount = promotion.getDiscount_value();
+        
+        	if (promotion.getPromotion_type().equals("CODE")) {
+        		double code = discount / (double)100;
+        		discount = (int) (price * code);
+        		finalPrice = (int) (price - discount);
+        	}else {
+        		finalPrice = price - discount;
+        	}        
+        	
+		if(finalPrice >= 10000) {
+			finalPrice = (int) Math.round(finalPrice / 1000.0) * 1000; 
+        
+        	return ProductAmountResponse.builder()
+                	.name(product.getName())
+                	.originPrice(price)
+                	.discountPrice(discount)
+                	.finalPrice(finalPrice)
+                	.build();
+        	
+        }else {
+        	System.out.println("할인된 최소 상품가격은 10,000 이상어이야 합니다.");
+        	return null;
+        	
+        }
+        
     }
+
 }
