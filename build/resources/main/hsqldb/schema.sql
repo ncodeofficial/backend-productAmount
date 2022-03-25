af4f09b78fbb444c523def37d580ec44e40e499f
Connor <kor.connor@gmail.com>
2022-03-25 23:24:45 +0900

    프로모션 가져오기 및 상품가격반환 추가

--- build/resources/main/hsqldb/schema.sql
+++ build/resources/main/hsqldb/schema.sql
@@ -7,14 +7,26 @@ CREATE TABLE product(
     PRIMARY KEY (id)
 );
 
-DROP TABLE coupon IF EXISTS ;
+DROP TABLE promotion IF EXISTS ;
 
-CREATE TABLE coupon(
+CREATE TABLE promotion(
     id INTEGER NOT NULL,
+    promotion_type VARCHAR(10),
     name VARCHAR(255),
     discount_type VARCHAR(15),
     discount_value INTEGER,
+    use_started_at DATE,
+    use_ended_at DATE,
     PRIMARY KEY (id)
 );
 
+
+DROP TABLE promotion_products IF EXISTS ;
+
+CREATE TABLE promotion_products(
+   id INTEGER NOT NULL,
+   promotion_id INTEGER,
+   product_id INTEGER,
+   PRIMARY KEY (id)
+);
 
--- src/main/java/dcode/service/ProductService.java
+++ src/main/java/dcode/service/ProductService.java
@@ -1,6 +1,7 @@
 package dcode.service;
 
 import dcode.domain.entity.Product;
+import dcode.domain.entity.Promotion;
 import dcode.model.request.ProductInfoRequest;
 import dcode.model.response.ProductAmountResponse;
 import dcode.repository.ProductRepository;
@@ -14,9 +15,27 @@ public class ProductService {
 
     public ProductAmountResponse getProductAmount(ProductInfoRequest request){
         System.out.println("상품 가격 추출 로직을 완성 시켜주세요.");
-
+        int discountedPrice = 0;
         Product product = repository.getProduct(request.getProductId());
 
-        return null;
+        for (int id: request.getCouponIds()) {
+            Promotion promotion = repository.getPromotion(id);
+            String promotionType = promotion.getPromotion_type();
+            int discountValue = promotion.getDiscount_value();
+
+            if (promotionType.equals("COUPON")) {
+                discountedPrice += discountValue;
+            } else if (promotionType.equals("CODE")) {
+                discountedPrice += product.getPrice() * discountValue / 100;
+            }
+        }
+
+        ProductAmountResponse productAmountResponse = ProductAmountResponse.builder()
+                .name(product.getName())
+                .originPrice(product.getPrice())
+                .discountPrice(discountedPrice)
+                .finalPrice(product.getPrice() - discountedPrice)
+                .build();
+        return productAmountResponse;
     }
 }
