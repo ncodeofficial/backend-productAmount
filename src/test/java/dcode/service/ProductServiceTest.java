0a05d6c4a9cdde7577468acf9f498fcf4db4bd11
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-25 09:19:57 +0900

    Refactor : 네이밍수정 ProductInfoRequest.couponIds -> promotionIds

--- src/test/java/dcode/service/ProductServiceTest.java
+++ src/test/java/dcode/service/ProductServiceTest.java
@@ -3,9 +3,7 @@ package dcode.service;
 import dcode.exception.NoSuchProductException;
 import dcode.model.request.ProductInfoRequest;
 import dcode.repository.ProductRepository;
-import dcode.repository.ProductRepositoryImpl;
 import dcode.repository.PromotionRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -37,7 +34,7 @@ class ProductServiceTest {
   @DisplayName("프로모션 적용된 상품가격 조회 : 존재하지 않는(DB에 없는) 프로덕트일 경우 NoSuchProductException 발생")
   void getProductAmount() {
     // given
-    var requestBody = ProductInfoRequest.builder().productId(1).couponIds(new int[]{1, 2}).build();
+    var requestBody = ProductInfoRequest.builder().productId(1).promotionIds(new int[]{1, 2}).build();
     // when
     when(productRepository.getProduct(requestBody.getProductId())).thenReturn(Optional.empty());
 
