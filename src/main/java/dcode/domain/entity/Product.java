c26704a9c8bf5b302341f541c1a4aa7d42b18376
seonpilKim <ksp970306@gmail.com>
2022-04-15 20:00:05 +0900

    Feat: 상품 가격 조회 로직 추가
    - SpringDataJPA 적용 -> 레거시 리팩토링
    - 쿠폰 적용 전 검증 로직
    - 할인 적용 금액이 10,000 미만이면, 10,000으로 설정
    - 최종 상품 금액 천 단위 절삭

--- src/main/java/dcode/domain/entity/Product.java
+++ src/main/java/dcode/domain/entity/Product.java
@@ -1,12 +1,30 @@
 package dcode.domain.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Data
-@Builder
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
-    private int id;
+
+    @Id
+    private Integer id;
+
     private String name;
+
     private int price;
+
+    @Builder
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
