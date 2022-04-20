d8b9e9e047cd68b838a4252444896f713544565e
sohee0724 <ayrs02117a@gmail.com>
2022-04-20 15:51:24 +0900

    chore: 테스트 데이터 INSERT 쿼리 추가

--- src/main/resources/hsqldb/data.sql
+++ src/main/resources/hsqldb/data.sql
@@ -1,8 +1,17 @@
 INSERT INTO promotion VALUES(1, 'COUPON', '30000원 할인쿠폰', 'WON', 30000, '2022-02-01', '2022-03-01' );
 INSERT INTO promotion VALUES(2, 'CODE', '15% 할인코드', 'PERCENT', 15, '2022-02-01', '2022-03-01');
+INSERT INTO promotion VALUES(3, 'CODE', '10% 할인코드', 'PERCENT', 10, '2022-02-01', '2022-03-01');
+INSERT INTO promotion VALUES(4, 'CODE', '5% 할인코드', 'PERCENT', 5, '2022-02-01', '2022-05-01');
+INSERT INTO promotion VALUES(5, 'CODE', '20% 할인코드', 'PERCENT', 20, '2022-02-01', '2022-05-01');
+INSERT INTO promotion VALUES(6, 'COUPON', '10000원 할인쿠폰', 'WON', 10000, '2022-02-01', '2022-05-01' );
+INSERT INTO promotion VALUES(7, 'COUPON', '5000원 할인쿠폰', 'WON', 5000, '2022-02-01', '2022-05-01' );
 
 INSERT INTO product VALUES(1, '디코드상품', 215000);
 
-
 INSERT INTO promotion_products VALUES(1, 1, 1);
 INSERT INTO promotion_products VALUES(2, 2, 1);
+INSERT INTO promotion_products VALUES(3, 2, 2);
+INSERT INTO promotion_products VALUES(4, 4, 1);
+INSERT INTO promotion_products VALUES(5, 5, 1);
+INSERT INTO promotion_products VALUES(6, 7, 1);
+INSERT INTO promotion_products VALUES(7, 7, 2);
